6e590393ab35152d97c563e933469e824b8469dc
adalentium <55710259+adalentium@users.noreply.github.com>
2021-05-14 12:02:18 +0300

    Update search_2.py

--- laba2/search_2.py
+++ laba2/search_2.py
@@ -1,40 +1,30 @@
-class BinarySearchMap:
-    def __init__(self):
-        self.data = [] # хранилище (key, value) значений
+def binary_search(list, item):
+  # в low и high хранятся границы части списка, где выполняется поиск
+  low = 0
+  high = len(list) - 1
+  i = 0
+  # Пока не останется один элемент
+  while low <= high:
+    # Проверяем средний элемент
+    mid = (low + high) // 2
+    guess = list[mid]
+    # Значение найдено
+    if guess == item:
+      return mid
+    # Значение велико
+    if guess > item:
+      high = mid - 1
+    # Значение мало
+    else:
+      low = mid + 1
+    i=i+1
+    print(i)
 
-    def search(self, key):
-        """ Поиск индекса (во всех случаях лучше левосторонний,
-            чтоб insert вставлял по убыванию) """
-        l = 0
-        r = len(self.data)
-        while l < r:
-            m = (l + r) // 2
-            if self.data[m][0] < key:
-                l = m + 1
-            else:
-                r = m
-        return l    
-        
-    def __setitem__(self, key, value):
-        """ Добавить элемент """
-        index = self.search(key)
-        # если ключ уже есть в таблице, то надо заменить значение
-        if index < len(self.data) and self.data[index][0] == key:
-            self.data[index] = (key, value)
-        else:
-            # иначе добавляем новую запись
-            self.data.insert(index, (key, value))
-    
-    def __delitem__(self, key):
-        """ Удалить элемент """
-        index = self.search(key)
-        self.data.pop(index)
-    
-    def __getitem__(self, key):
-        """ Получить элемент """
-        index = self.search(key)
-        found_key, val = self.data[index]
-        # если найденный индекс выдает запрашиваемый ключ
-        if found_key == key:
-            return val
-        raise KeyError()
+  # Значение не найдено
+  return None
+
+my_list = [1, 3, 5, 7, 9]
+print(binary_search(my_list, 3)) # => 1 (позиция элемента в списке)
+
+# 'None' в Python означает "ничто". Элемент не найден.
+print(binary_search(my_list, -1)) # => None
